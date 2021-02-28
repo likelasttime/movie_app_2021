c252123ccf29f336f4ed2ccad912cfd4704aec94
likelasttime <a50070863@gmail.com>
2021-02-28 22:07:29 +0900

    Redirecting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {HashRouter, Route} from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
 import Detail from "./routes/Detail";
@@ -9,10 +9,10 @@ import "./App.css";
 function App(){
     return(
         <HashRouter>
-            <Navigation/>
+            <Navigation />
             <Route path="/" exact={true} component={Home} />
             <Route path="/about" component={About} />
-            <Route path="/movie-detail" component={Detail}/>
+            <Route path="/movie/:id" component={Detail} />
         </HashRouter>
     );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,11 +3,13 @@ import {Link} from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({year, title, summary, poster, genres}){
+function Movie({id, year, title, summary, poster, genres}){
+    console.log(id);
     return(
+        <div className="movie">
         <Link
         to={{
-            pathname:"/movie-detail",
+            pathname: `/movie/${id}`,
             state:{
                 year,
                 title,
@@ -17,7 +19,6 @@ function Movie({year, title, summary, poster, genres}){
             }
         }}
         >
-            <div className="movie">
             <img src={poster} alt={title} title={title} />
             <div className="movie__data">
                 <h3 className="movie__title">{title}</h3>
@@ -31,8 +32,8 @@ function Movie({year, title, summary, poster, genres}){
                 </ul>
                 <p className="movie__summary">{summary.slice(0, 180)}...</p>
             </div>
-        </div>
         </Link>
+        </div>
     );
 }
 
